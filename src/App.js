335b8fb2b49a5235ccce650621664a24bf829993
Paul Obegi <pauloh@Pauls-MacBook-Pro.local>
2023-06-28 04:58:46 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+// import Navbar from "./Components/navbar/Example";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Navbar/>*/}
+        <Hero/>
+        <About/>
+    </div>
+  );
+}
+
+export default App;
