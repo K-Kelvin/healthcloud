adb48e42badea8351e19fc7070a59e0b4bf4bd03
pauloh-ui <paulonyambu7@gmail.com>
2023-06-28 05:18:21 +0300

    Added h-screen to hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -13,7 +13,7 @@ function Hero() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <div className="bg-white">
+        <div className="bg-white h-screen">
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
