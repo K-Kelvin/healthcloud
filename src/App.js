288f4fbe313b21374505c6858f86820ed6ab0cab
pauloh-ui <paulonyambu7@gmail.com>
2023-06-29 05:10:22 +0300

    Added some routing functionality to the page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 // import Navbar from "./Components/navbar/Example";
+import { Routes, Route } from "react-router-dom";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Login from "./Components/login/Login";
+import SignIn from "./Components/login/SignIn";
 
 function App() {
   return (
     <div className="App">
       {/*<Navbar/>*/}
-        <Hero/>
-        <About/>
+        <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signin" element={<SignIn />} />
+        </Routes>
+
     </div>
   );
 }
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import {Link} from "react-router-dom";
 
 const navigation = [
     { name: 'Product', href: '#' },
@@ -17,14 +18,14 @@ function Hero() {
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
+                        <Link to="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                 alt=""
                             />
-                        </a>
+                        </Link>
                     </div>
                     <div className="flex lg:hidden">
                         <button
@@ -44,9 +45,9 @@ function Hero() {
                         ))}
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/signin" className="text-sm font-semibold leading-6 text-gray-900">
                             Log in <span aria-hidden="true">&rarr;</span>
-                        </a>
+                        </Link>
                     </div>
                 </nav>
                 <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
